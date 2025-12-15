594399d10d081fa3b597a0a7fc44dbcba430dd6f
Ilya-Bife <ilya2017.dolgov@yandex.ru>
2025-12-15 10:08:24 +0500

    Task6

--- Tyuiu.DolgovIV.Sprint6.Task6.V29/FormMain.cs
+++ Tyuiu.DolgovIV.Sprint6.Task6.V29/FormMain.cs
@@ -12,36 +12,36 @@ namespace Tyuiu.DolgovIV.Sprint6.Task6.V29
         }
         private void buttonDone_Click(object sender, EventArgs e)
         {
-            try
-            {
+            //try
+            //{
 
-                dataGridViewNums.ColumnCount = 2;
-                dataGridViewNums.Columns[0].Width = 20;
-                dataGridViewNums.Columns[1].Width = 50;
-
-
-
-                this.chartFunction.ChartAreas[0].AxisX.Title = "Ось X";
-                this.chartFunction.ChartAreas[0].AxisY.Title = "Ось Y";
-
-                this.chartFunction.Series[0].Points.Clear();
-
-                double[] numMass = new double[ds.len];
-
-                numMass = ds.LoadFromDataFile(path);
-
-                for (global::System.Int32 i = 0; i < numMass.Length; i++)
-                {
-                    dataGridViewNums.Rows.Add(Convert.ToString(i), Convert.ToString(numMass[i]));
-                    chartFunction.Series[0].Points.AddXY(i, numMass[i]);
-                }
+            //    dataGridViewNums.ColumnCount = 2;
+            //    dataGridViewNums.Columns[0].Width = 20;
+            //    dataGridViewNums.Columns[1].Width = 50;
 
 
-            }
-            catch
-            {
-                MessageBox.Show("Введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
+
+            //    this.chartFunction.ChartAreas[0].AxisX.Title = "Ось X";
+            //    this.chartFunction.ChartAreas[0].AxisY.Title = "Ось Y";
+
+            //    this.chartFunction.Series[0].Points.Clear();
+
+            //    double[] numMass = new double[ds.len];
+
+            //    numMass = ds.LoadFromDataFile(path);
+
+            //    for (global::System.Int32 i = 0; i < numMass.Length; i++)
+            //    {
+            //        dataGridViewNums.Rows.Add(Convert.ToString(i), Convert.ToString(numMass[i]));
+            //        chartFunction.Series[0].Points.AddXY(i, numMass[i]);
+            //    }
+
+
+            //}
+            //catch
+            //{
+            //    MessageBox.Show("Введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            //}
         }
     }
 }
